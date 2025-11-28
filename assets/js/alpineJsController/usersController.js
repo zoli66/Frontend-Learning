d159825c82e5eaceb9bf53a0e8cdc6817ef3a881
Zolfaghar_A <abbasi.z1366@yahoo.com>
2025-11-28 19:14:31 +0330

    part7-delete&updateCompleted

--- assets/js/alpineJsController/usersController.js
+++ assets/js/alpineJsController/usersController.js
@@ -10,11 +10,13 @@ document.addEventListener("alpine:init", () => {
     showAddModal: false,
     searchChar: "",
     newUserInfo: {
+      id: 0,
       name: "",
       username: "",
       email: "",
       address: "",
     },
+    userIdToEdit: null,
     getUsers() {
       this.isLoading = true;
       axios
@@ -81,12 +83,13 @@ document.addEventListener("alpine:init", () => {
         .post("https://jsonplaceholder.typicode.com/users", this.newUserInfo)
         .then((res) => {
           if (res.status == 201) {
+            this.newUserInfo.id = res.data.id;
             this.mainUsers.push(this.newUserInfo);
             this.pagination();
             this.showAddModal = false;
             this.handleResetForm();
             M.toast({
-              html: "کاربر با موفقیت ایجاد شد",
+              html: "Create User Successfully",
               classes: "rounded green",
             });
           }
@@ -102,6 +105,68 @@ document.addEventListener("alpine:init", () => {
         email: "",
         address: "",
       };
+    },
+    handleDeleteUser(user) {
+      var toastHTML = `<span>Are You Sure To Delete ${user.name}</span><button class="btn-flat toast-action" x-on:click="handleConfirmDelete(${user.id})">Delete</button>`;
+      M.toast({ html: toastHTML });
+    },
+    handleConfirmDelete(userId) {
+      this.isLoading = true;
+      axios
+        .delete("https://jsonplaceholder.typicode.com/users/" + userId)
+        .then((res) => {
+          if (res.status == 200) {
+            this.mainUsers = this.mainUsers.filter((user) => user.id != userId);
+            this.users = this.mainUsers;
+            this.pagination();
+            M.toast({
+              html: "User Deleted Successfully",
+              classes: "green",
+            });
+          }
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    handleUpdateUser(user) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users/" + user.id)
+        .then((res) => {
+          if (res.status == 200) {
+            this.newUserInfo = {
+              name: res.data.name,
+              username: res.data.username,
+              email: res.data.email,
+              address: res.data.address.city,
+            };
+            this.showAddModal = true;
+            this.userIdToEdit = res.data.id;
+          }
+        });
+    },
+    handleConfirmEditUser() {
+      axios
+        .put(
+          "https://jsonplaceholder.typicode.com/users/" + this.userIdToEdit,
+          this.newUserInfo
+        )
+        .then((res) => {
+          if (res.status == 200) {
+            let userIndex = this.mainUsers.findIndex(
+              (user) => user.id == this.userIdToEdit
+            );
+            this.mainUsers[userIndex] = res.data;
+            this.handleResetForm();
+            this.showAddModal = false;
+            this.pagination();
+            this.userIdToEdit = null;
+            M.toast({
+              html: "User Updated Successfully",
+              classes: "green",
+            });
+          }
+        });
     },
   }));
 });
